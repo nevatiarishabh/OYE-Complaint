4da6807a7754b57714e49add269fefb752180cf9
Nishant Dave <davenishant2k@gmail.com>
2020-07-14 15:50:58 +0530

    updated database url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   // 'url': 'mongodb+srv://admin:admin@listenup.mecwk.gcp.mongodb.net/test2?retryWrites=true&w=majority'
-  'url': 'mongodb://localhost:27017/complaint'
+  // 'url': 'mongodb://localhost:27017/complaint'
+  'url': process.env.ATLAS_URL
 };
