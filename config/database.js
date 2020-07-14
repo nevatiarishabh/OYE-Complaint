86a3539ed90cd3847516fe989e4f5203eae390fc
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2020-07-14 19:05:21 +0530

    DatabaseJS changes

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
   // 'url': 'mongodb+srv://admin:admin@listenup.mecwk.gcp.mongodb.net/test2?retryWrites=true&w=majority'
   // 'url': 'mongodb://localhost:27017/complaint'
