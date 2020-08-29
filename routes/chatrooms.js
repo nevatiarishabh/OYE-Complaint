70b84fe4f73e92b00b503ca7359766df134f6b1d
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2020-08-29 23:03:04 +0530

    get routes in chatrooms.js

--- routes/chatrooms.js
+++ routes/chatrooms.js
@@ -13,5 +13,69 @@ router.use(express.static(path.join(__dirname + 'public')));
 router.get('/',function(req,res){
     res.render("organization_chat.ejs");
 });
+
+router.get('/post', isLoggedIn, function(req, res) {
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Posts'
+    });
+  });
+  
+  router.get('/health', isLoggedIn, function(req, res) {
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Health & Family Welfare'
+    });
+  });
+  
+  router.get('/external_affair', isLoggedIn, function(req, res) {
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'External Affair'
+    });
+  });
+  
+  router.get('/banking', isLoggedIn, function(req, res) {
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Banking'
+    });
+  });
+  
+  router.get('/insurance', isLoggedIn, function(req, res){
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Insurance'
+    });
+  });
+  
+  router.get('/telecoms', isLoggedIn, function(req, res){
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Telecoms'
+    });
+  });
+  
+  router.get('/road', isLoggedIn, function(req, res){
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Road Transport And Highways'
+    });
+  });
+  
+  router.get('/schooledu', isLoggedIn, function(req, res){
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'School Education and Literacy'
+    });
+  });
+  
+  router.get('/petroleum', isLoggedIn, function(req, res){
+    res.render('index2.ejs', {
+      user: req.user,
+      Ministry: 'Petroleum and Natural Gas'
+    });
+  });
+
 module.exports = router;
 
